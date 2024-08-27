dbdc5dabfe700a5a7adc0d96a607807bdf96ad58
William Spanfelner <wil-1-am@outlook.com>
2024-08-27 16:25:24 +0300

    Print to debug area to see contents of a variable

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -9,3 +9,13 @@ name = "Keeley"
 
 // Constants are declared with let and it's value does not change
 let character = "Daphne"
+
+// Print to debug area (at bottom) to see a variable's value
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
