b07a185eb01cfa61484a65e857ee97907a97d25f
William Spanfelner <wil-1-am@outlook.com>
2024-08-28 21:39:43 +0300

    Negate a boolean with !

--- iOS17-Day02.playground/Contents.swift
+++ iOS17-Day02.playground/Contents.swift
@@ -1,3 +1,15 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Booleans store true or false
+let goodDogs = true
+let gameOver = false
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
