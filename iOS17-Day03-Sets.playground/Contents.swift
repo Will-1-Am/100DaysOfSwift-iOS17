73d99f92c4fe06397a99ff616c2135a5d6ded685
William Spanfelner <wil-1-am@outlook.com>
2024-08-29 18:08:59 +0300

    Create a set

--- iOS17-Day03-Sets.playground/Contents.swift
+++ iOS17-Day03-Sets.playground/Contents.swift
@@ -1,4 +1,10 @@
 import Cocoa
 
 //How to use sets for fast data lookup
-
+// Define a set
+let actors = Set([
+    "Denzel Washington",
+    "Tom Cruise",
+    "Nicolas Cage",
+    "Samuel L Jackson",
+])
