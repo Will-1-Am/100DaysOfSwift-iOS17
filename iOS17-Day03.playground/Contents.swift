588c0d8dfdda51a102d28312e0d903270ad9d26e
William Spanfelner <wil-1-am@outlook.com>
2024-08-29 13:17:43 +0300

    Access array elements

--- iOS17-Day03.playground/Contents.swift
+++ iOS17-Day03.playground/Contents.swift
@@ -5,4 +5,9 @@ var beatles = ["John", "Paul", "George", "Ringo"]
 let numbers = [4, 8, 15, 16, 23, 42]
 var temperatures = [25.3, 28.2, 26.4]
 
+// Reading array are done by index
+beatles[0]
+numbers[3]
+temperatures[2]
+// NOTE: If index does not exist within the array - crashy crashy!
 
