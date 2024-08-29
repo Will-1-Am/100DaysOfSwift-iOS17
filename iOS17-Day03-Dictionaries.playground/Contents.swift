b928c8417c2117346fbbe58253390652842ed75b
William Spanfelner <wil-1-am@outlook.com>
2024-08-29 17:18:02 +0300

    Define a dictionary of key/value pairs

---
+++ iOS17-Day03-Dictionaries.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+//How to store and find data in dictionaries
+//Define a dictionary
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville",
+]
+
