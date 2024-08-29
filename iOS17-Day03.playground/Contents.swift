9406409dc4c15c81cc0d58468528428752cdbf3d
William Spanfelner <wil-1-am@outlook.com>
2024-08-29 12:50:58 +0300

    Create arrays of different types string, integer, double

---
+++ iOS17-Day03.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+//How to store ordered data in arrays
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+
