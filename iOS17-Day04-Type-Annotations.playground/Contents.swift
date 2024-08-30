ffaa01687cf785c0657e586bcd07f889ffad823d
William Spanfelner <wil-1-am@outlook.com>
2024-08-30 20:53:30 +0300

    Annotate data type examples

--- iOS17-Day04-Type-Annotations.playground/Contents.swift
+++ iOS17-Day04-Type-Annotations.playground/Contents.swift
@@ -9,3 +9,7 @@ let score = 0
 // The data type can be set explicitly
 let name: String = "Fred"
 let gear: Int = 4
+
+//Some common examples
+let balance: Double = 0
+var isAuthenticated: Bool = true
