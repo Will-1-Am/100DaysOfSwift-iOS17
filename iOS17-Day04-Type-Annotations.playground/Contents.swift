2757d16155d4b79fa51ed77fc610f93a1f5bf54d
William Spanfelner <wil-1-am@outlook.com>
2024-08-30 20:59:52 +0300

    Examples of more complex data

--- iOS17-Day04-Type-Annotations.playground/Contents.swift
+++ iOS17-Day04-Type-Annotations.playground/Contents.swift
@@ -13,3 +13,13 @@ let gear: Int = 4
 //Some common examples
 let balance: Double = 0
 var isAuthenticated: Bool = true
+
+//More complex examples
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set([
+    "The Bluest Eye",
+    "Foundation",
+    "Girl, Woman, Other"
+])
+//Type annotation is not strictly required since Swift will work out the type from the data
