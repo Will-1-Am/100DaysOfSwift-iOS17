a93fc9d750b8ae131251f652800244f4a11dbc96
William Spanfelner <wil-1-am@outlook.com>
2024-08-31 22:01:50 +0300

    Define switch with a default case

--- iOS17-Day05-Switch-Statements.playground/Contents.swift
+++ iOS17-Day05-Switch-Statements.playground/Contents.swift
@@ -38,3 +38,16 @@ case .snow:
 case .unknown:
     print("Our forecast generator is broken.")
 }
+
+
+let place = "Metropolis"
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
