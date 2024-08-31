ca1dedaac6362b41b3382ad78dc391d3c388fa2f
William Spanfelner <wil-1-am@outlook.com>
2024-08-31 19:17:48 +0300

    Create sample conditionals

--- iOS17-Day05-Conditionals.playground/Contents.swift
+++ iOS17-Day05-Conditionals.playground/Contents.swift
@@ -11,3 +11,19 @@ if score > 80 {
     print("Great job!")
 }
 
+// Some very basic examples
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going, wed don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote.")
+}
