9c114572927bb717dd0a5ae6a1762b2199a9f78a
William Spanfelner <wil-1-am@outlook.com>
2024-08-31 21:53:48 +0300

    Define switch statement

--- iOS17-Day05-Switch-Statements.playground/Contents.swift
+++ iOS17-Day05-Switch-Statements.playground/Contents.swift
@@ -18,3 +18,23 @@ if forecast == .sun {
 } else {
     print("Our forecast generator is broken.")
 }
+
+// Problems with the above if/eles if /else:
+//1. forecast is repeated many times;
+//2. .rain case is checked twice by accident;
+//3. .snow case isn't checked;
+
+// These problems can be avoided with a switch statement
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm.")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken.")
+}
