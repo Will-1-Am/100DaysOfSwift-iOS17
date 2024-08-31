3200b6bbdc31b4c117fe5acb3fa539348f46d5b3
William Spanfelner <wil-1-am@outlook.com>
2024-08-31 21:33:26 +0300

    Create a more complex example

--- iOS17-Day05-Multiple-Conditions.playground/Contents.swift
+++ iOS17-Day05-Multiple-Conditions.playground/Contents.swift
@@ -41,3 +41,19 @@ let hasParentalConsent = true
 if age >= 18 || hasParentalConsent {
     print("You can buy the game!")
 }
+
+// A more complex example
+enum TransportOption {
+case airplane, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.airplane
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path.")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
