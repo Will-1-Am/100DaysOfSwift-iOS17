3e1386da90874dda11e534ee8df416a0bf6dc518
William Spanfelner <wil-1-am@outlook.com>
2024-09-01 20:49:30 +0300

    Complete Checkpoint 3

--- iOS17-Day06-Skip-Loop-Items.playground/Contents.swift
+++ iOS17-Day06-Skip-Loop-Items.playground/Contents.swift
@@ -29,3 +29,19 @@ for i in 1...100_000 {
     }
 }
 print(multiples)
+
+
+// Checkpoint 3 - FizzBuzz
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+
