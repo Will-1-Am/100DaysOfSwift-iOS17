b483e711126dd81c09ca41abcf61df6cb565c214
William Spanfelner <wil-1-am@outlook.com>
2024-09-01 19:55:20 +0300

    Iterate while a condition is true

---
+++ iOS17-Day06-While-Loops.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+//How to use a while loop to repeat work
+//Iterate while a condition is true
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
