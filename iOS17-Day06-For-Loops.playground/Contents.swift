593f78a5468e8bcdc71ff7c1c57a02cf1a0fddbb
William Spanfelner <wil-1-am@outlook.com>
2024-09-01 19:17:03 +0300

    Iterate over a range

--- iOS17-Day06-For-Loops.playground/Contents.swift
+++ iOS17-Day06-For-Loops.playground/Contents.swift
@@ -6,3 +6,9 @@ let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
 for os in platforms {
     print("Swift works great on \(os).")
 }
+
+
+//Iterate over a range
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
