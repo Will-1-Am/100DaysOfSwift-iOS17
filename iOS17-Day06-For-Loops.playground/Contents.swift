c6dfd6a4daf5ad0ffd4125635397f4abe870ec47
William Spanfelner <wil-1-am@outlook.com>
2024-09-01 19:27:11 +0300

    Loop over non-inclusive range

--- iOS17-Day06-For-Loops.playground/Contents.swift
+++ iOS17-Day06-For-Loops.playground/Contents.swift
@@ -24,3 +24,13 @@ for i in 1...12 {
     
     print()
 }
+
+//Loop over an inclusive range 1 to 5
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+//Loop over a non-inclusive range 1 up to 5
+for i in 1..<5 {
+    print("Counting from 1 up to 5: \(i)")
+}
