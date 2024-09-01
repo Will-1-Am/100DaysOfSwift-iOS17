818cb78e45a10c8636b4e8d9f1a9b67292175cad
William Spanfelner <wil-1-am@outlook.com>
2024-09-01 20:29:47 +0300

    Exit loop immediately with break

--- iOS17-Day06-Skip-Loop-Items.playground/Contents.swift
+++ iOS17-Day06-Skip-Loop-Items.playground/Contents.swift
@@ -13,3 +13,19 @@ for filename in filenames {
     }
     print("Found picture: \(filename)")
 }
+
+
+// Example: exit loop immediately when condition is met
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+print(multiples)
