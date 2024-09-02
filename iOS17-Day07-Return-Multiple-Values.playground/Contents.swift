fec2c5d7cd983070801373302c6ae4bd86d14a53
William Spanfelner <wil-1-am@outlook.com>
2024-09-02 22:17:17 +0300

    Example of non-ideal return of multiple values

--- iOS17-Day07-Return-Multiple-Values.playground/Contents.swift
+++ iOS17-Day07-Return-Multiple-Values.playground/Contents.swift
@@ -6,3 +6,12 @@ func isUppercase(string: String) -> Bool {
 }
 
 isUppercase(string: "fred")
+
+func getUser() -> [String] {
+    ["Taylor", "Swift"]
+}
+
+let user = getUser()
+print("Name: \(user[0]) \(user[1])")
+
+//This works, but not ideal - what do the indices mean and what if the array changes?
