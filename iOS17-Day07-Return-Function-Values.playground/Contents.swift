b3e9a83fdb6a85fa83e050768ab3919c4082424d
William Spanfelner <wil-1-am@outlook.com>
2024-09-02 21:53:51 +0300

    Create a function to compare strings

--- iOS17-Day07-Return-Function-Values.playground/Contents.swift
+++ iOS17-Day07-Return-Function-Values.playground/Contents.swift
@@ -11,3 +11,18 @@ func rollDice() -> Int {
 
 let result = rollDice()
 print(result)
+
+
+// Do two strings contain the same letters, regardless of their order?
+// This function should:
+// - Accept two string parameters
+// - Return true if their letters are the same
+
+func compareStrings(first: String, second: String) -> Bool {
+    if first.sorted() == second.sorted() {
+        return true
+    }
+    return false
+}
+
+compareStrings(first: "abc", second: "cabd")
