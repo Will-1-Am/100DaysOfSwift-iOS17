7a85ee588000d8498c87ecec8898988a3608cf50
William Spanfelner <wil-1-am@outlook.com>
2024-09-02 21:12:26 +0300

    Create an example function

---
+++ iOS17-Day07-Functions.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+//How to reuse code with functions
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+
+// func is the keyword the identifies a function
+// func is followed by a name and parentheses that contain parameters, if any
+// Curly brackets surround the function body - the actions that will be completed when the function is called
+
+// To run the function, it is called by its name with open & closed parentheses
+
+showWelcome()
