39490d1bb062670557c37b3c291b5330a35db62d
William Spanfelner <wil-1-am@outlook.com>
2024-09-03 14:40:35 +0300

    Document 3 step process for handling errors

---
+++ iOS17-Day08-Error-Handling-In-Functions.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+//How to handle errors in functions
+//Define all error that might happen in the code
+//Write a function that works as normal, but can throw one or more of those errors if a serious problem is found
+//Run the function handling the errors that come back
+
+
