1141c7032b554b470d3e8c799ad6e90481fcc2f9
William Spanfelner <wil-1-am@outlook.com>
2024-09-03 14:43:57 +0300

    Define likely errors in an enum

--- iOS17-Day08-Error-Handling-In-Functions.playground/Contents.swift
+++ iOS17-Day08-Error-Handling-In-Functions.playground/Contents.swift
@@ -5,4 +5,8 @@ import Cocoa
 //Write a function that works as normal, but can throw one or more of those errors if a serious problem is found
 //Run the function handling the errors that come back
 
-
+// Example - Let's check a users password and say whether it is good or bad
+// Step 1 - Define errors that are likely
+enum PasswordError: Error {
+    case short, obvious
+}
