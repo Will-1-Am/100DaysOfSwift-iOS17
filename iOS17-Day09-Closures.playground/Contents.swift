a283d5f1d7045d3b0de34912abd03a0e27eecaad
William Spanfelner <wil-1-am@outlook.com>
2024-09-04 21:49:29 +0300

    Create a closure

--- iOS17-Day09-Closures.playground/Contents.swift
+++ iOS17-Day09-Closures.playground/Contents.swift
@@ -8,3 +8,12 @@ func greetUser() {
 greetUser()
 var greetCopy = greetUser  // Creating a copy of the function the () are not included.  If the () are included the assignment means that the function should be executed and the value stored in greetCopy.
 greetCopy()
+
+
+// What if you'd just like to assign some functionality to a variable or a constant
+let sayHello = {
+    print("Hi there!")
+}
+
+sayHello()
+// The above is called a closure
