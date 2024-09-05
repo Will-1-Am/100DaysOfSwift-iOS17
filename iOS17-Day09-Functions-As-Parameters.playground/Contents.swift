275496e980616cdeea2c12f546c95737b83c680a
William Spanfelner <wil-1-am@outlook.com>
2024-09-05 13:08:27 +0300

    Create a function that takes a function as parameter

---
+++ iOS17-Day09-Functions-As-Parameters.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+//How to accept functions as parameters
+
+//Write a function that generates an array of integers by calling another function repeatedly
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    
+    return numbers
+}
+
