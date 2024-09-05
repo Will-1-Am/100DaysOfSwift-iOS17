8ea50b61a9643ad0bcd12c583c5ec639e30c2966
William Spanfelner <wil-1-am@outlook.com>
2024-09-05 14:02:19 +0300

    Print each array elemet to a single line

--- iOS17-Day09-Functions-As-Parameters.playground/Contents.swift
+++ iOS17-Day09-Functions-As-Parameters.playground/Contents.swift
@@ -56,4 +56,4 @@ doImportantWork {
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
-luckyNumbers.filter { $0 % 2 != 0 }.sorted { $0 < $1 }.map { "\(String($0)) is a lucky number" }
+luckyNumbers.filter { $0 % 2 != 0 }.sorted { $0 < $1 }.map { "\(String($0)) is a lucky number" }.forEach { print($0) }
