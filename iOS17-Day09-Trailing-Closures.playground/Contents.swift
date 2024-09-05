b84b128d28fa8ca94f0560158401c202048a4b1d
William Spanfelner <wil-1-am@outlook.com>
2024-09-05 12:36:18 +0300

    Remove wrapping parentheses

--- iOS17-Day09-Trailing-Closures.playground/Contents.swift
+++ iOS17-Day09-Trailing-Closures.playground/Contents.swift
@@ -10,15 +10,18 @@ print(sortedTeam)  // ["Gloria", "Piper", "Suzanne", "Tasha", "Tiffany"]
 
 /*
  The sorted method demands that a function/closure supplied for a
- custom sort, takes two strings and returns a Bool.  Since our closure fits these requirements, Swift allows this to be written:
+ custom sort, takes two strings and returns a Bool.  Since our closure fits these requirements, Swift allows this to be written without the types.
+ 
+ When the closure is supplied as the parameter for another function, as is the case with sorted, the wrapping parentheses may be ommitted like so.
+ 
  */
-let captainFirstTeam = team.sorted(by: { a, b in
+let captainFirstTeam = team.sorted { a, b in
     if a == "Suzanne" {
         return true
     } else if b == "Suzanne" {
         return false
     }
     return a < b
-})
+}
 
 print(captainFirstTeam)  //["Suzanne", "Gloria", "Piper", "Tasha", "Tiffany"]
