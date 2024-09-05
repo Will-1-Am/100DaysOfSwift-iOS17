4783f3899fbac9b57ac738524bede23f5e46ed24
William Spanfelner <wil-1-am@outlook.com>
2024-09-05 11:50:35 +0300

    Create an inline closure to use with the sorted method

--- iOS17-Day09-Closures.playground/Contents.swift
+++ iOS17-Day09-Closures.playground/Contents.swift
@@ -44,7 +44,7 @@ print(user)
 // Why this matters?
 let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
 let sortedTeam = team.sorted()
-print(sortedTeam)
+print(sortedTeam)  // ["Gloria", "Piper", "Suzanne", "Tasha", "Tiffany"]
 
 /*
  Write a custom sorting function for sorted() that makes the
@@ -68,5 +68,17 @@ func captainFirstSorted(name1: String, name2: String) -> Bool {
     return name1 < name2
 }
 
-let captainFirstTeam = team.sorted(by: captainFirstSorted)
-print(captainFirstTeam)
+//let captainFirstTeam = team.sorted(by: captainFirstSorted)
+//print(captainFirstTeam)  //["Suzanne", "Gloria", "Piper", "Tasha", "Tiffany"]
+
+// Converting the captainFirstTeam constant with a closure looks like this.
+let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    return name1 < name2
+})
+
+print(captainFirstTeam)  //["Suzanne", "Gloria", "Piper", "Tasha", "Tiffany"]
