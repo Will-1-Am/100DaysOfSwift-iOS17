d41ae5b0d95b3ac9f880689a1ddaaa7286d8c36c
William Spanfelner <wil-1-am@outlook.com>
2024-09-06 19:10:52 +0300

    Set a default value for a struct property

--- iOS17-Day10-Structs.playground/Contents.swift
+++ iOS17-Day10-Structs.playground/Contents.swift
@@ -27,7 +27,7 @@ wings.printSummary()
 /* All properties in structs are constant.  To mutate them, their mutating functions must be marked mutating */
 struct Employee {
     let name: String
-    var vacationRemaining: Int
+    var vacationRemaining: Int = 14
     
     mutating func takeVacation(days: Int) {
         if vacationRemaining > days {
@@ -41,7 +41,7 @@ struct Employee {
     
 }
 
-// An instance of a struct with a mutating function needs to be created as a variable not a constant
+// An instance of a struct using a mutating function needs to be created as a variable not a constant
 var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
 archer.takeVacation(days: 5)
 print(archer.vacationRemaining)
@@ -50,3 +50,8 @@ print(archer.vacationRemaining)
 let a = 1
 let b = 2.0
 let c = Double(a) + b   // Double is a Swift struct
+
+let kane = Employee(name: "Lana Kane")
+var poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+poovey.takeVacation(days: 10)
+print(poovey.vacationRemaining)
