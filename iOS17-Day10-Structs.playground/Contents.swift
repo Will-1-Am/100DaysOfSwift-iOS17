44207db2156ec9e27a2e2195c875b1bc1a6c045f
William Spanfelner <wil-1-am@outlook.com>
2024-09-06 18:15:34 +0300

    Access properties of the struct instances

--- iOS17-Day10-Structs.playground/Contents.swift
+++ iOS17-Day10-Structs.playground/Contents.swift
@@ -16,3 +16,10 @@ struct Album {
 
 let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
 let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
