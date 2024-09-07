934059b31a3fbcb5ab09e7bdc6f44b8a1c6e6298
William Spanfelner <wil-1-am@outlook.com>
2024-09-07 22:09:33 +0300

    Create struct with default initializer

---
+++ iOS17-Day10-Create-Custom-Initializers.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+// How to create custom initializers
+//Initializers are special functions inside structs designed to create initial values for all the properties inside the struct.
+//By the time the initializer finish you must ensure that every property in the struct has a value.
+
+struct Player {
+    let name: String
+    let number: Int
+}
+
+let player = Player(name: "Megan R", number: 15)
+// The above uses the memberwise initializer - a fancy way of saying it accepts property values in the order in which they were defined.
