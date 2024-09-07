3fb2d0d72c3f996c57f7c21a00c2deedfec089df
William Spanfelner <wil-1-am@outlook.com>
2024-09-07 19:27:57 +0300

    Create computed property

--- iOS17-Day10-Dynamic-Property-Values.playground/Contents.swift
+++ iOS17-Day10-Dynamic-Property-Values.playground/Contents.swift
@@ -15,4 +15,21 @@ print(archer.vacation)
 archer.vacation -= 3
 print(archer.vacation)
 
+//This works but with the loss of the original value assignment
 
+struct NewEmployee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        vacationAllocated - vacationTaken
+    }
+}
+
+var s_archer = NewEmployee(name: "Sterling Archer", vacationAllocated: 14)
+
+s_archer.vacationTaken += 4
+print(s_archer.vacationRemaining)
+s_archer.vacationTaken += 4
+print(s_archer.vacationRemaining)
