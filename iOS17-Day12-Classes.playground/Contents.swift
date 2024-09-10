ee79e78103ae0c99dad71572adb1b5a6be60418d
William Spanfelner <wil-1-am@outlook.com>
2024-09-10 13:24:13 +0300

    Create a class & an instance of it

--- iOS17-Day12-Classes.playground/Contents.swift
+++ iOS17-Day12-Classes.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
 //How to create your own classes
+// Classes are another way that types can be created.  They differ from structs in five key ways, but they also have similarities with structs:
+// - You get to create and name them;
+// - Add properties, methods, property observers, and access control;
+// - Create custom initializers to configure new instances.
+// Classes differ from structs as follows:
+// - You can make one class build upon functionality in another class;
+// - Swift won't generate a memberwise initializer for classes;
+// - If you copy an instance of a class, both copies share the same data;
+// - We can addd a deinitializer to run when the final copy is destroyed;
+// - Constant class instances can have their variable properties changed;
+
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
