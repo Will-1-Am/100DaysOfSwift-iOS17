3060e00530afc212ee62e4968cdc70f1face4913
William Spanfelner <wil-1-am@outlook.com>
2024-09-10 15:09:36 +0300

    Call parent initializer to instantiate child class

--- iOS17-Day12-Add-Initializers-to-Classes.playground/Contents.swift
+++ iOS17-Day12-Add-Initializers-to-Classes.playground/Contents.swift
@@ -14,7 +14,14 @@ class Vehicle {
 class Car: Vehicle {
     let isConvertible: Bool
     
-    init(isConvertible: Bool) {
+    init(isElectric: Bool, isConvertible: Bool) {
         self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
     }
 }
+
+// The child class cannot be created unless the parent class has a value for all of its variables.  This means our child's custom initializer must supply the values for the parent (in this case).
+
+let car = Car(isElectric: true, isConvertible: false)
+car.isConvertible
+car.isElectric
