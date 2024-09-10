3c00e361fa4e31933982937a2f81f977a379e45c
William Spanfelner <wil-1-am@outlook.com>
2024-09-10 15:38:13 +0300

    Make a manual unique copy of an instance

--- iOS17-Day12-Copy-Classes.playground/Contents.swift
+++ iOS17-Day12-Copy-Classes.playground/Contents.swift
@@ -4,10 +4,17 @@ import Cocoa
 // Copies of classes always point to the same data, because they are reference types.
 class User {
     var username = "Anonymous"
+    
+//    If we really want to avoid the default behaviour the following method could be added. Sometimes this is called a deep copy.
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
 }
 
 var user1 = User()
-var user2 = user1
+var user2 = user1.copy()
 user2.username = "Taylor"
 print(user1.username)
 print(user2.username)
