b264e8b1e2fa58f75558e9e7d4cb27c397ce53a2
William Spanfelner <wil-1-am@outlook.com>
2024-09-10 15:12:06 +0300

    Example of child class with no custom initializer

--- iOS17-Day12-Add-Initializers-to-Classes.playground/Contents.swift
+++ iOS17-Day12-Add-Initializers-to-Classes.playground/Contents.swift
@@ -12,16 +12,11 @@ class Vehicle {
 }
 
 class Car: Vehicle {
-    let isConvertible: Bool
-    
-    init(isElectric: Bool, isConvertible: Bool) {
-        self.isConvertible = isConvertible
-        super.init(isElectric: isElectric)
-    }
+    let isConvertible = false
 }
 
 // The child class cannot be created unless the parent class has a value for all of its variables.  This means our child's custom initializer must supply the values for the parent (in this case).
 
-let car = Car(isElectric: true, isConvertible: false)
+let car = Car(isElectric: true)
 car.isConvertible
 car.isElectric
