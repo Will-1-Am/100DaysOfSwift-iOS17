af3efb62acadc3a38137e11366828359581024da
William Spanfelner <wil-1-am@outlook.com>
2024-09-12 16:03:17 +0300

    Amend extension to apply to protocol

--- iOS17-Day13-Get-the-Most-from-Protocol-Extensions.playground/Contents.swift
+++ iOS17-Day13-Get-the-Most-from-Protocol-Extensions.playground/Contents.swift
@@ -1,11 +1,15 @@
 import Cocoa
 
 /// How to get the most from protocol extensions
-extension Int {
-    func squared() -> Int {
+extension Numeric {
+    func squared() -> Self {
         self * self
     }
 }
 
 let wholeNumber = 5
 print(wholeNumber.squared())
+/// If we want the same functionality on Double we can extend the protocol that Int & Double conform to.  Numeric is such a protocol.
+/// However, this causes a problem since a Double squared cannot be an Int & vice versa.
+/// The Self keyword can help us here as the return type since it refers to the conforming type that we call this thing on.
+/// If we call this on an Int, it will return an Int and likewise for a Double.
