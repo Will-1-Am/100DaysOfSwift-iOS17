4886fe57392bf33e2be17f5afb183a1c927cb9b5
William Spanfelner <wil-1-am@outlook.com>
2024-09-12 15:21:20 +0300

    Check if an array is empty

---
+++ iOS17-Day13-Protocol-Extensions.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+/// How to create and use protocol extensions
+/// Protocol extensions extend functionality of protocols meaning that any types conforming to the protocol will automatically get the extended functionality.
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isEmpty == false {
+    print("Guest count: \(guests.count)")
+}
+
+if !guests.isEmpty {
+    print("Guest count: \(guests.count)")
+}
+
