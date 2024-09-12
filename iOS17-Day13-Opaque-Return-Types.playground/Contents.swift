a68562d000a1681a0749f45b55797200f7e217d4
William Spanfelner <wil-1-am@outlook.com>
2024-09-12 12:49:13 +0300

    Add explainer text

--- iOS17-Day13-Opaque-Return-Types.playground/Contents.swift
+++ iOS17-Day13-Opaque-Return-Types.playground/Contents.swift
@@ -14,3 +14,13 @@ func getRandomBool() -> some Equatable {
 /// Both Int & Bool conform to the Equatable protocol which allows us to use '==' to compare values of the same type.
 print(getRandomNumber() == getRandomNumber())
 
+/// Since Int & Bool conform to Equatable, we should be able to substitute Equatable for their respective return types in the function definitions above.  But if the subsitution is made Swift will not permit it, while it is ok generally to use protocols for return types.  Understanding the reason behind this exception is key to understanding opaque return types.
+/// If we try to compare '5' to 'true' for instance, this is clearly nonsense which is the reason why Swift doesn't allow Equatible as a return type.  In order for us to use Equatable as a return type it must be preceded with the some keyword
+
+let num = getRandomNumber()
+
+/// Swift always understands what the underlying type is allowing us to sort of 'hide' this information in our code through the use of protocols.  The 'some' keyword allows us to be less specific when declaring the return type.
+
+/// SwiftUI uses the View protocol extensively for buttons, tabbar, navbar, etc.  Using this protocol allows Swift to decide what to return.
+
+protocol View { }
