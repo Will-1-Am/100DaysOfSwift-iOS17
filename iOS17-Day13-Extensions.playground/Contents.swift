758906092023c628bb2df7cb6dd2640a5a3d5122
William Spanfelner <wil-1-am@outlook.com>
2024-09-12 14:26:50 +0300

    Create an extension to String that performs the trimming

--- iOS17-Day13-Extensions.playground/Contents.swift
+++ iOS17-Day13-Extensions.playground/Contents.swift
@@ -8,3 +8,11 @@ var quote = "       The truth is rarely pure and never simple    "
 /// To remove the white space before the capital 'T' and after the final 'e', we can write the following:
 
 let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+/// These methods come from the Foundation api, but they are a bit wordy.  We could write the following to simplify the call.
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
