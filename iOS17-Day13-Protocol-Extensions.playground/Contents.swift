4f02e81fb0e0fb0078cf04f068a83cf6d36469e4
William Spanfelner <wil-1-am@outlook.com>
2024-09-12 15:22:54 +0300

    Create extension to Array

--- iOS17-Day13-Protocol-Extensions.playground/Contents.swift
+++ iOS17-Day13-Protocol-Extensions.playground/Contents.swift
@@ -2,6 +2,11 @@ import Cocoa
 
 /// How to create and use protocol extensions
 /// Protocol extensions extend functionality of protocols meaning that any types conforming to the protocol will automatically get the extended functionality.
+extension Array {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
 
 let guests = ["Mario", "Luigi", "Peach"]
 
@@ -12,4 +17,3 @@ if guests.isEmpty == false {
 if !guests.isEmpty {
     print("Guest count: \(guests.count)")
 }
-
