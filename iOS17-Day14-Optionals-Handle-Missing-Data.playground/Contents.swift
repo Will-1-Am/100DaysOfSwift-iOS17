1716f6d102058e910705ba7036a78f4a3bae6eae
William Spanfelner <wil-1-am@outlook.com>
2024-09-18 17:36:27 +0300

    Unwrap an optional

--- iOS17-Day14-Optionals-Handle-Missing-Data.playground/Contents.swift
+++ iOS17-Day14-Optionals-Handle-Missing-Data.playground/Contents.swift
@@ -9,3 +9,10 @@ let peachOpposite = opposites["Peach"]
 /// The big question with the value of peachOpposite above is what will it be? It's obvious that opposites does not contain the "Peach" key, but Swift will have to read the dictionary to find out.
 /// In this case, since the dictionary is of type String: String, the value type is optional and indicated with a question mark like this: String?
 /// All data types could have a optional variation.
+
+
+/// To read optional data the syntax below is used:
+if let marioOpposite = opposites["Mario"] {
+    print("Mario opposite is \(marioOpposite)")
+}
+/// The above is called unwrapping.
