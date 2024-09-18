3e1de9e95fc4fe20e52cf001dde437a0e2e45057
William Spanfelner <wil-1-am@outlook.com>
2024-09-18 18:24:09 +0300

    Create example unwrappings

--- iOS17-Day14-Optionals-Unwrap-With-Guard.playground/Contents.swift
+++ iOS17-Day14-Optionals-Unwrap-With-Guard.playground/Contents.swift
@@ -11,3 +11,14 @@ func printSquare(of number: Int?) {
     }
     print("\(number) x \(number) is \(number * number)")
 }
+
+/// Consider the following:
+var myVar: Int? = 3
+
+if let unwrapped = myVar {
+    /// Run if myVar has a value inside
+}
+
+guard let unwrapped = myVar else {
+    /// Run if myVar doesn't have a value inside
+}
