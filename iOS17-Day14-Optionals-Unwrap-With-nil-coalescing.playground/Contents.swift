9845c9c9800c9e782a740264f0b936c71999d07c
William Spanfelner <wil-1-am@outlook.com>
2024-09-19 21:21:47 +0300

    Select random element from array

--- iOS17-Day14-Optionals-Unwrap-With-nil-coalescing.playground/Contents.swift
+++ iOS17-Day14-Optionals-Unwrap-With-nil-coalescing.playground/Contents.swift
@@ -12,3 +12,7 @@ let new = captains["Serenity"] ?? "N/A"
 print(type(of: new))  // Optional<String> w/o ??  String with ?? "N/A"
 /// If there was a value assigned to the key, it would be returned instead.
 
+///Nil coalescing works with other optional types also.
+let tvShows = ["Archers", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"  ///The randomElement method will return an optional string if one exists
+
