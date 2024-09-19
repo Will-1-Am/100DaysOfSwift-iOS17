a379a393e0223f97759086ea250a549b2c0bea1a
William Spanfelner <wil-1-am@outlook.com>
2024-09-19 20:30:26 +0300

    Access a non-existant dictionary key

--- iOS17-Day14-Optionals-Unwrap-With-nil-coalescing.playground/Contents.swift
+++ iOS17-Day14-Optionals-Unwrap-With-nil-coalescing.playground/Contents.swift
@@ -1,8 +1,13 @@
 import Cocoa
 
-//How to unwrap optionals with nil coalescing
+///How to unwrap optionals with nil coalescing
+///The nil coalescing operator will provide a default value for a nil value
 let captains = [
     "Enterprise": "Picard",
     "Voyager": "Janeway",
     "Defiant": "Sisko"
 ]
+
+let new = captains["Serenity"]
+print(type(of: new))  // Optional<String>
+
