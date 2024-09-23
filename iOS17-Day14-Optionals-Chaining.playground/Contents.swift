4ae7b4c85360fc8942718f1bb380e219aa2d5eee
William Spanfelner <wil-1-am@outlook.com>
2024-09-23 16:28:51 +0100

    Create an array, select a random element and apply uppercase to it.

---
+++ iOS17-Day14-Optionals-Chaining.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+/// How to handle multiple optionals using optional chaining
+/// Optional chaining is a simplified syntax for reading optionals inside optionals
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+
+
