632c1b722a46950902c998094ab2bcc8345a1ea7
William Spanfelner <wil-1-am@outlook.com>
2024-09-23 16:55:44 +0100

    Complete example code with comments

--- iOS17-Day14-Optionals-Chaining.playground/Contents.swift
+++ iOS17-Day14-Optionals-Chaining.playground/Contents.swift
@@ -5,7 +5,18 @@ import Cocoa
 let names = ["Arya", "Bran", "Robb", "Sansa"]
 let chosen = names.randomElement()?.uppercased() ?? "No one"
 print("Next in line: \(chosen)")
+/// The assignment to chosen says, pick a randomElement, unwrap it and change it to uppercase. If it's nil assign "No one".
 
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
+
+///The assignment to author says, if there is a book and it has an author and it has a first character then uppercase the string and assign it to author.
 
 
 
