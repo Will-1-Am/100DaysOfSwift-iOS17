05ee37afc1c17e0b20f93fe847bb7e0ea408c962
William Spanfelner <wil-1-am@outlook.com>
2024-09-24 11:35:28 +0100

    Combine optional try with nil coalesscing

--- iOS17-Day14-Optionals-Function-Failure.playground/Contents.swift
+++ iOS17-Day14-Optionals-Function-Failure.playground/Contents.swift
@@ -14,3 +14,6 @@ if let user = try? getUser(id: 23) {
 }
 /// try? will not reveal the reason for the failure, a normal try would need to be used for that.
 /// try? can be combined with nil-coalescing
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
