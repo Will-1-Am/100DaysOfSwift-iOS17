c51c9bcacc266ba07884c874c5ff580eb04c0fc1
William Spanfelner <wil-1-am@outlook.com>
2024-09-25 17:04:10 +0100

    Add quote & boolean toggle examples

---
+++ iOS17-Day15-Swift-Review.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+/// Swift Review
+/// Quotes - to include quotes within a quote preceed the quotation mark with a back slash
+let quote = "He tapped a sign saying \"Beleive\" and walked away."
+
+/// Toggle a boolean value
+var skyIsBlue = false
+skyIsBlue.toggle()
