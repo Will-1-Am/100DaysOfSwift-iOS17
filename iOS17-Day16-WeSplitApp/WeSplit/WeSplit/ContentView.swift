684fdf51c977744700825834ca0faded960919db
William Spanfelner <wil-1-am@outlook.com>
2024-10-04 17:02:13 +0100

    Use private access control with state property wrappers to signal that the property is created & used within this entity

--- iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
+++ iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    @State private var tapCount = 0
+    
     var body: some View {
         Button ("Tap Count: \(tapCount)") {
             tapCount += 1
