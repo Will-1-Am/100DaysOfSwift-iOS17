f0f732838a9d3336d39085d6a96f5d93c4f47b17
William Spanfelner <wil-1-am@outlook.com>
2024-10-06 09:52:51 +0100

    Reset code completely

--- iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
+++ iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron", "Draco"]
-    @State private var selectedStudent = "Harry"
-    
     var body: some View {
         Text("Hello, World!")
     }
