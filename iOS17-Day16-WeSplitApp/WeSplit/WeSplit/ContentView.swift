24544a62b7e2583a2683dbf4c29087a270bbf2b9
William Spanfelner <wil-1-am@outlook.com>
2024-10-06 10:08:10 +0100

    Create array of tip rates

--- iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
+++ iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    /// The following array will allow people to tip at different rates
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         Text("Hello, World!")
     }
