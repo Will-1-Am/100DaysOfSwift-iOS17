b51ae8e591c45f6b6ba2e13ee8f0a4714d9b0113
William Spanfelner <wil-1-am@outlook.com>
2024-10-06 10:29:59 +0100

    Grab the user's local currency from the Locale struct

--- iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
+++ iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     var body: some View {
         Form {
             Section {
-                TextField("Enter the amount of the cheque", value: $chequeAmount, format: .currency(code: "USD"))
+                TextField("Enter the amount of the cheque", value: $chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
             }
         }
     }
