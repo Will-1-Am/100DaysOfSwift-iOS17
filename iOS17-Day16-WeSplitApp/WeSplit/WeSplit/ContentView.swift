157fae8a051d800524afad2380beeb578ef6cc9b
William Spanfelner <wil-1-am@outlook.com>
2024-10-11 11:51:18 +0100

    Add navlink pickerStyle & wrap entire form in a NavigationStack

--- iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
+++ iOS17-Day16-WeSplitApp/WeSplit/WeSplit/ContentView.swift
@@ -16,18 +16,21 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Enter the amount of the cheque", value: $chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-                Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2..<100) {
-                        Text("\($0) people")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter the amount of the cheque", value: $chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    .pickerStyle(.navigationLink)
                 }
-            }
-            Section {///The following text feild simply reflects the value stored in the variable as it is stored.
-                Text(chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section {///The following text feild simply reflects the value stored in the variable as it is stored.
+                    Text(chequeAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
         }
     }
