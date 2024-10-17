7eefce3e22d4bf8d1b90125799039e8f56d0f499
William Spanfelner <wil-1-am@outlook.com>
2024-10-17 17:21:59 +0100

    Create new branch to save previous work.

---
+++ iOS17-Day19-UnitConverterApp/UnitConverterApp/UnitConverterApp/UnitConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterAppApp.swift
+//  UnitConverterApp
+//
+//  Created by William Spanfelner on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
