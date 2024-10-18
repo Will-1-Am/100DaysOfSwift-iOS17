260378991ded68c7425c042dd6723d1798f41e6f
William Spanfelner <wil-1-am@outlook.com>
2024-10-18 15:47:19 +0100

    Add project starting files

---
+++ UnitConvertor/UnitConvertor/UnitConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertorApp.swift
+//  UnitConvertor
+//
+//  Created by William Spanfelner on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitConvertor/UnitConvertorTests/UnitConvertorTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertorTests.swift
+//  UnitConvertorTests
+//
+//  Created by William Spanfelner on 18/10/2024.
+//
+
+import Testing
+@testable import UnitConvertor
+
+struct UnitConvertorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
