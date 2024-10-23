7822ac911cf41c5ef248c129f55887a960974a79
William Spanfelner <wil-1-am@outlook.com>
2024-10-23 22:56:56 +0100

    Wrap the Section in a Form element

--- iOS17-Day19-UnitConvertorApp/UnitConvertor/UnitConvertor/ContentView.swift
+++ iOS17-Day19-UnitConvertorApp/UnitConvertor/UnitConvertor/ContentView.swift
@@ -11,14 +11,13 @@ struct ContentView: View {
     @State private var inputValue = 0.0
     
     var body: some View {
-        Section {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            TextField("Enter a value", value: $inputValue, format: .number)
-                .keyboardType(.numberPad)
+        Form {
+            Section {
+                TextField("Enter a value", value: $inputValue, format: .number)
+                    .keyboardType(.numberPad)
+            }
         }
-        .padding()
+
     }
 }
 
